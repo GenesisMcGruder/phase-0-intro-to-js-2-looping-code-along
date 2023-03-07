de18005ccdb54326f5a71e6470fe8e96fc8ca00f
Genesis McGruder <genesismcgruder@gmail.com>
2023-03-07 00:17:43 -0500

    commit tree c3125792f9d2ee654d1bdced4e47ce649ba3323c

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i <gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names,event) {
+    let message = [];
+    for (let i = 0; i < names.length; i++) {
+     message.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return message;
+}
+ 
+function countDown(positiveNumber) {
+    while (positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+    return countDown;
+}
